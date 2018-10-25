04b1e9d0507e47379f1cda885a2e10afc51d0ddf
ozgukaya <oozgukaya@gmail.com>
2018-10-26 00:27:50 +0300

    Camera Settings and Proje Platformu
    Project platform changed to Android.
    Camera Settings and camera effect shake added.

---
+++ Assets/Scripts/ShakeTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTrigger : MonoBehaviour {
+
+    public CameraShake cameraShake;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(cameraShake.Shake(.15f, .1f));
+
+        }
+    }
+}
