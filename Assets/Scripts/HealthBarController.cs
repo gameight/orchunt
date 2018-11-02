fc6f7acdf66d68b82cbd86364261bd90e3055086
Ümit Serbest <umitserbest@gmail.com>
2018-11-02 15:43:02 +0300

    InGame UI

---
+++ Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+    public float health;
+
+    Image healthBar;
+    float maxHealth = 100f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        healthBar.fillAmount = health / maxHealth;
+	}
+}
