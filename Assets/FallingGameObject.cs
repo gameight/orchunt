a8b63aa8417c422cb971eb5e9e14ebb09b9f56cb
Serhat Colak <serhat26basak@gmail.com>
2018-11-11 01:48:45 +0300

    Level Design

---
+++ Assets/FallingGameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGameObject : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Invoke("DropPlatform", 0.2f);
+            Destroy(gameObject, 1f);
+        }
+    }
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ Assets/Pendulum.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+
+    public Rigidbody2D body2d;
+    public float leftPushRange;
+    public float rightPushRange;
+    public float velocityThreshold;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        body2d.angularVelocity = velocityThreshold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Push();
+    }
+
+    public void Push()
+    {
+        if(transform.rotation.z >0 && transform.rotation.z < rightPushRange && (body2d.angularVelocity > 0) && body2d.angularVelocity < velocityThreshold)
+        {
+            body2d.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0 && transform.rotation.z > leftPushRange && (body2d.angularVelocity < 0 ) && body2d.angularVelocity >  velocityThreshold * -1)
+        {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
