9bff36d0f8c6c31e394c8bb0c5445a902c02b107
Serhat Colak <serhat26basak@gmail.com>
2018-11-11 23:54:12 +0300

    LevelDesignTest_Update

---
+++ Assets/Scripts/AboveFallingObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboveFallingObject : MonoBehaviour {
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+            rb.isKinematic = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals(""))
+            Invoke("DropPlatform", 0.1f);
+            Destroy(gameObject, 0.1f);
+    }
+
+}
---
+++ Assets/Scripts/SpearController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public Vector2 dir = new Vector2(-10, 0);
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = dir;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player") || collision.gameObject.name.Equals("Crate"))
+        {
+            Destroy(gameObject, 0f);
+        }
+    }
+}
