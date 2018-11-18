9dce60bdbd941c0466e1ae6373b13e225be023ce
Özgü Kaya <oozgukaya@gmail.com>
2018-11-19 00:44:23 +0300

    level2 moving platforms added

---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour {
+
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transformB;
+
+    void Start () {
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+		
+	}
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+
+        if(Vector3.Distance(childTransform.localPosition,nextPos)<= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
