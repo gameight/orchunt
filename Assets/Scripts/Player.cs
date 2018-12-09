7b78e6eaedf7af596145158f7f7e0eb74bafed11
Özgü Kaya <oozgukaya@gmail.com>
2018-12-09 19:25:28 +0300

    game over system and checkpoint added, some bugs fixed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [System.Serializable]
+    public class PlayerStats{
+       
+        public int Health = 100;
+
+    }
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary =-15;
+
+    private void Update()
+    {
+        if (transform.position.y <= fallBoundary)
+            DamagePlayer(999);
+    }
+
+    public void DamagePlayer(int damage){
+        playerStats.Health -= damage;
+
+        if(playerStats.Health<=0){
+            GameMaster.killPlayer(this);
+        }
+    } 
+
+}
