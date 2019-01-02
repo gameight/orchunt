b80fe7b4472d1f11eebb8e747d3a27cf8da5eb95
xFireBerg <32838402+xFireBerg@users.noreply.github.com>
2019-01-02 20:55:09 +0300

    added basic level end connection
    ToDo:
    //add boss healthcheck before transitioning
    mapScene level selection

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") )//&& GameObject.FindGameObjectWithTag("Boss").GetComponent<EnemyAI>().health == 0)
+        {   
+            SceneManager.LoadScene("MapScene");
+        }
+    }
+}
