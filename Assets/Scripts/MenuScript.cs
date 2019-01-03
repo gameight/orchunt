561841c937c8109e03c3e94057cba40aef15dfc1
Bekir Sefa BAL <sefabal@outlook.com>
2019-01-04 02:44:04 +0300

    Rina's dead and cast animations added, after dead animations it needs to wait for 1.5 second. Tried to add scene connection to mapScene but button script doesn't work.'

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    public void portalClicked(GameObject game)
+    {
+        Debug.Log("test");
+        Debug.Log(game.name);
+    }
+
+    void Update () {
+		
+	}
+}
