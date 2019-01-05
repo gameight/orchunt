9883b381581fd4bfbd6f0a1994dfdc120d7a3140
xFireBerg <32838402+xFireBerg@users.noreply.github.com>
2019-01-05 20:02:53 +0300

    Added Remaining Lives UI

---
+++ Assets/RemainingLivesUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+public class RemainingLivesUI : MonoBehaviour {
+
+   
+    private Text livesText;
+
+	// Use this for initialization
+	void Awake () {
+        livesText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        livesText.text = "x" + SaveSystem.playerData.remainingLive.ToString();
+	}
+}
